c55f566548dd1a60e8298830269298fc9ff97cd0
Ginfranco Groppo <gf.groppo@gmail.com>
2020-12-17 21:51:52 -0300

    proyecto nuevo php poo

---
+++ proyecto-php-poo/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Nectarina Clothes</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="assets/css/styles.css" rel="stylesheet">
+    </head>
+    <body>
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/logo/logo.png" alt="Logo">
+                <a href="index.php">Nectarina Clothes</a>
+            </div>
+        </header>
+
+        <!-- Menu -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 1</a>
+                </li><li>
+                    <a href="index.php">Categoria 2</a>
+                </li><li>
+                    <a href="index.php">Categoria 3</a>
+                </li><li>
+                    <a href="index.php">Categoria 3</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="content">
+            <!--Side bar -->
+            <aside id="sidebar">
+                <div id="login" class="block_aside">
+                    <form action="#" method="POST">
+                        <label for="email">Email</label>
+                        <input type="text" name="email">
+
+                        <label for="password">Password</label>
+                        <input type="password" name="password">
+
+                        <input type="submit" value="Login">
+                    </form>
+
+                    <a href="#">Mis pedidos</a>
+                    <a href="#">Gestionar pedidos</a>
+                    <a href="#">Gestionar categorias</a>
+                </div>
+            </aside>
+
+
+            <!-- GENERAL CONTENT -->
+            <div id="central">
+
+                <div class="product">
+                    <img src="assets/img/productos/DSC_4926.jpg" alt="">
+                    <h2>Nombre del producto</h2>
+                    <p>Precio del producto</p>
+                    <a href="#">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/productos/DSC_4926.jpg" alt="">
+                    <h2>Nombre del producto</h2>
+                    <p>Precio del producto</p>
+                    <a href="#">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/productos/DSC_4926.jpg" alt="">
+                    <h2>Nombre del producto</h2>
+                    <p>Precio del producto</p>
+                    <a href="#">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/productos/DSC_4926.jpg" alt="">
+                    <h2>Nombre del producto</h2>
+                    <p>Precio del producto</p>
+                    <a href="#">Comprar</a>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Footer -->
+        <footer id="footer">
+            <p>Developed by GG Design &copy 2020</p>
+        </footer>
+
+
+
+
+
+    </body>
+</html>
